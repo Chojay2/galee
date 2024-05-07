c8c5f849f37eee85cf0aac240a8e8e00865a2b33
Chojay2 <chojay.wangchuk@selise.ch>
2024-05-07 13:35:24 +0600

    init commit
    Signed-off-by: Chojay2 <chojay.wangchuk@selise.ch>

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      sss
+      <Image src={"/1.png"} width={660} height={400} alt="flower-1 img"></Image>
+    </main>
+  );
+}
